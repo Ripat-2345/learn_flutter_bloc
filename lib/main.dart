5cd622adbf3e900b657fd54fb7fb0f4832ec8105
Ripat-2345 <ripatalsafar@gmail.com>
2022-11-01 14:39:30 +0700

    basic cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,17 +10,70 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage(),
     );
   }
 }
 
+class CounterCubit extends Cubit<int> {
+  CounterCubit({this.inisialData = 0}) : super(inisialData);
+
+  int inisialData;
+
+  void tambahData() {
+    emit(state + 1);
+  }
+
+  void kurangData() {
+    emit(state - 1);
+  }
+}
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  CounterCubit myCounter = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CUBIT APPS"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder(
+            initialData: myCounter.inisialData,
+            stream: myCounter.stream,
+            builder: (context, snapshot) {
+              return Text(
+                snapshot.data.toString(),
+                style: const TextStyle(fontSize: 50),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  myCounter.kurangData();
+                },
+                icon: const Icon(Icons.remove),
+              ),
+              IconButton(
+                onPressed: () {
+                  myCounter.tambahData();
+                },
+                icon: const Icon(Icons.add),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }

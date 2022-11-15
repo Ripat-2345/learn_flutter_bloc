fa9667a32e90c386bbaffe1995b78b2ac9f16032
Ripat-2345 <ripatalsafar@gmail.com>
2022-11-15 09:10:54 +0700

    Bloc Builder Done

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '/bloc/counter.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,16 +16,36 @@ class HomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          StreamBuilder(
-            stream: mycounter.stream,
-            builder: (context, snapshot) {
+          // * Widget yg disediakan oleh flutter_bloc
+          BlocBuilder<Counter, int>(
+            bloc: mycounter,
+            buildWhen: (previous, current) {
+              if (current % 2 == 0) {
+                return true;
+              } else {
+                return false;
+              }
+            },
+            builder: (context, state) {
               return Text(
-                snapshot.data.toString(),
+                state.toString(),
                 style:
                     const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
               );
             },
           ),
+
+          // * Widget bawaan flutter
+          // StreamBuilder(
+          //   stream: mycounter.stream,
+          //   builder: (context, snapshot) {
+          //     return Text(
+          //       snapshot.data.toString(),
+          //       style:
+          //           const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
+          //     );
+          //   },
+          // ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [

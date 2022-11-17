095982b63a33fc4cea92557475d5f6c56a780f3d
Ripat-2345 <ripatalsafar@gmail.com>
2022-11-17 09:01:21 +0700

    Bloc Consumer

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -8,40 +8,44 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // * BlocListener
+    // * BLocConsumer
     return Scaffold(
       appBar: AppBar(
-        title: const Text("BLOC BUILDER"),
+        title: const Text("BLOC CONSUMER"),
         centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // * Widget yg disediakan oleh flutter_bloc
-          BlocListener(
+          BlocConsumer<Counter, int>(
             bloc: mycounter,
+            buildWhen: (previous, current) {
+              if (current >= 10) {
+                return true;
+              } else {
+                return false;
+              }
+            },
+            builder: (context, state) {
+              return Text(
+                state.toString(),
+                style:
+                    const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
+              );
+            },
             listener: (context, state) {
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(content: Text("SUDAH MENCAPAI 15 NIH")),
               );
             },
             listenWhen: (previous, current) {
-              if (current == 15) {
+              if (current % 2 == 0) {
                 return true;
               } else {
                 return false;
               }
             },
-            child: BlocBuilder<Counter, int>(
-              bloc: mycounter,
-              builder: (context, state) {
-                return Text(
-                  state.toString(),
-                  style: const TextStyle(
-                      fontSize: 38, fontWeight: FontWeight.w700),
-                );
-              },
-            ),
           ),
 
           Row(
@@ -70,6 +74,69 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+
+    // * BlocListener
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text("BLOC LISTENER"),
+    //     centerTitle: true,
+    //   ),
+    //   body: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       // * Widget yg disediakan oleh flutter_bloc
+    //       BlocListener(
+    //         bloc: mycounter,
+    //         listener: (context, state) {
+    //           ScaffoldMessenger.of(context).showSnackBar(
+    //             const SnackBar(content: Text("SUDAH MENCAPAI 15 NIH")),
+    //           );
+    //         },
+    //         listenWhen: (previous, current) {
+    //           if (current == 15) {
+    //             return true;
+    //           } else {
+    //             return false;
+    //           }
+    //         },
+    //         child: BlocBuilder<Counter, int>(
+    //           bloc: mycounter,
+    //           builder: (context, state) {
+    //             return Text(
+    //               state.toString(),
+    //               style: const TextStyle(
+    //                   fontSize: 38, fontWeight: FontWeight.w700),
+    //             );
+    //           },
+    //         ),
+    //       ),
+
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //         children: [
+    //           IconButton(
+    //             onPressed: () {
+    //               mycounter.decrement();
+    //             },
+    //             icon: const Icon(
+    //               Icons.remove,
+    //               size: 30,
+    //             ),
+    //           ),
+    //           IconButton(
+    //             onPressed: () {
+    //               mycounter.increment();
+    //             },
+    //             icon: const Icon(
+    //               Icons.add,
+    //               size: 30,
+    //             ),
+    //           )
+    //         ],
+    //       )
+    //     ],
+    //   ),
+    // );
 
     // * BlocBuilder
     // return Scaffold(

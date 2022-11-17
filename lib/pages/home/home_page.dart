1d2ee310ec4193c33364da7236fd50ef68e12032
Ripat-2345 <ripatalsafar@gmail.com>
2022-11-17 08:34:41 +0700

    Bloc Listener

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // * BlocListener
     return Scaffold(
       appBar: AppBar(
         title: const Text("BLOC BUILDER"),
@@ -17,35 +18,32 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // * Widget yg disediakan oleh flutter_bloc
-          BlocBuilder<Counter, int>(
+          BlocListener(
             bloc: mycounter,
-            buildWhen: (previous, current) {
-              if (current % 2 == 0) {
+            listener: (context, state) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("SUDAH MENCAPAI 15 NIH")),
+              );
+            },
+            listenWhen: (previous, current) {
+              if (current == 15) {
                 return true;
               } else {
                 return false;
               }
             },
-            builder: (context, state) {
-              return Text(
-                state.toString(),
-                style:
-                    const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
-              );
-            },
+            child: BlocBuilder<Counter, int>(
+              bloc: mycounter,
+              builder: (context, state) {
+                return Text(
+                  state.toString(),
+                  style: const TextStyle(
+                      fontSize: 38, fontWeight: FontWeight.w700),
+                );
+              },
+            ),
           ),
 
-          // * Widget bawaan flutter
-          // StreamBuilder(
-          //   stream: mycounter.stream,
-          //   builder: (context, snapshot) {
-          //     return Text(
-          //       snapshot.data.toString(),
-          //       style:
-          //           const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
-          //     );
-          //   },
-          // ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -72,5 +70,71 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+
+    // * BlocBuilder
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text("BLOC BUILDER"),
+    //     centerTitle: true,
+    //   ),
+    //   body: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       // * Widget yg disediakan oleh flutter_bloc
+    //       BlocBuilder<Counter, int>(
+    //         bloc: mycounter,
+    //         buildWhen: (previous, current) {
+    //           if (current % 2 == 0) {
+    //             return true;
+    //           } else {
+    //             return false;
+    //           }
+    //         },
+    //         builder: (context, state) {
+    //           return Text(
+    //             state.toString(),
+    //             style:
+    //                 const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
+    //           );
+    //         },
+    //       ),
+
+    //       // * Widget bawaan flutter
+    //       // StreamBuilder(
+    //       //   stream: mycounter.stream,
+    //       //   builder: (context, snapshot) {
+    //       //     return Text(
+    //       //       snapshot.data.toString(),
+    //       //       style:
+    //       //           const TextStyle(fontSize: 38, fontWeight: FontWeight.w700),
+    //       //     );
+    //       //   },
+    //       // ),
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //         children: [
+    //           IconButton(
+    //             onPressed: () {
+    //               mycounter.decrement();
+    //             },
+    //             icon: const Icon(
+    //               Icons.remove,
+    //               size: 30,
+    //             ),
+    //           ),
+    //           IconButton(
+    //             onPressed: () {
+    //               mycounter.increment();
+    //             },
+    //             icon: const Icon(
+    //               Icons.add,
+    //               size: 30,
+    //             ),
+    //           )
+    //         ],
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
